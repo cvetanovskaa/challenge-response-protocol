fccaf8db166c509c6d82c8fe0d0fc6e59b60e427
cvetanovskaa <cvetanovskaa@berea.edu>
2018-11-24 18:14:47 -0500

    Made the basic layout of the client side. We still need to make sure it send & recieves the correct values

---
+++ client.py
@@ -0,0 +1,38 @@
+'''
+ The client is receving a random hashed value from the server & combining it with the password the user enters.
+ Then, that is being sent to the server. If it matches the value that the server gets, then the authentication is valid.
+'''
+
+import socket, sys, time
+from Crypto.Cipher import AES
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ('127.0.0.1', 8000)
+
+
+def hashFunction(input):
+	''' Used to return a hash value of the password & the password+serverVal '''
+	return hash(input)
+
+def getHashVal():
+	message = socks.recv(1024)  
+	#print (message.decode('ASCII'))
+	return message
+
+def sendData(data):
+	sock.send(data + "\n")
+	return
+
+def main():
+	server.connect(server_address)
+	#Maybe make a login/registration part?
+
+	uname = raw_input("Enter your username")
+	pass = raw_input("Enter your password")
+	serverVal = getHashVal()
+	finalHashVal = hashFunction(serverVal + pass)
+
+	send(uname)
+	send(finalHashVal)
+	
+	
