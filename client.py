d01e73fb7d3205925e9a33e3ed9216b022570567
tirthasubedi <subedi.tirtha@gmail.com>
2018-11-28 22:38:58 -0500

    upatingn

--- client.py
+++ client.py
@@ -2,36 +2,49 @@
  The client is receving a random hashed value from the server & combining it with the password the user enters.
  Then, that is being sent to the server. If it matches the value that the server gets, then the authentication is valid.
 '''
-
 import socket, sys, time
 from Crypto.Cipher import AES
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('127.0.0.1', 8000)
-
+listening_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sending_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ADDRESS, PORT = '127.0.0.1', 8000
+listening_sock.connect((ADDRESS, PORT+1))
+sending_sock.connect((ADDRESS,PORT))
 
 def hashFunction(input):
 	''' Used to return a hash value of the password & the password+serverVal '''
 	return hash(input)
 
 def getHashVal():
-	message = socks.recv(1024)  
-	message = message.decode('ASCII')
-	return message
+# 	message = listening_sock.recv(1024)  
+# 	message = message.decode('ASCII')
+# 	return message
+
+
+# def Tcp_Read( ):
+	a = ' '
+	b = ''
+	while a != '\n':
+		a = listening_sock.recv(1)
+		b = b + a
+	return b
 
 def sendData(data):
-	sock.send(data + "\n")
+	data = data + "\n"
+	sending_sock.send(data.encode('ASCII'))
 	return
 
 def main():
-	server.connect(server_address)
 	#Maybe make a login/registration part?
 
-	uname = raw_input("Enter your username")
-	passw = raw_input("Enter your password")
+	uname = input("Enter your username")
+	passw = input("Enter your password")
+	sendData("")
+	print("A")
 	serverVal = getHashVal()
-
+	print(serverVal)
 	finalHashVal = hashFunction(serverVal + passw)
 
 	send((uname,finalHashVal))
 
+main()
--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 import socket
 import sqlite3
+import sys
 
-HOST, PORT = 'localhost', 8000
+HOST, PORT = '127.0.0.1', 8000
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
+
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+send_sock.bind((HOST, PORT+1))
 
 unames = [("subedir", "berea2015"), ("karkig", "berea2016"), ("scottw", "college201"),("alex", "berea2018"), ("vasant","berea2010"),
  ("peter", "berea2011"), ("ronaldo", "realmadrid"), ("messi", "barcelona"), ("neymar", "psg2017")]
@@ -41,7 +46,17 @@ def add_user(db, username, password):
 	for uname in unames:
 		cursor.execute('insert into usernames (username, password) values (?, ?)', (uname[0], uname[1],))
 #		cursor.execute('insert into usernames (name, password) values (?, ?)', (username, password,))
-	conn.commit() 	
+	conn.commit()
+
+def Tcp_Read():
+	a = ' '
+	b = ''
+	#import pdb
+	#pdb.set_trace()
+	while a != '\n':
+		a = listen_socket.recv(1)
+		b = b + a
+	return b
 
 def main():
 
@@ -53,16 +68,43 @@ def main():
 			username nvarchar(40) not null,
 			password nvarchar(32) not null
 		); """
-	
+
 	if conn is not None:
 		create_table(conn, sql_query)
 #		add_user(db, "","")
 	else:
 		print("Error! cannot create the database connection.")
+	
+
+	# making 
+
+	listen_socket.listen(1)
 
 	while True:
-		
-	
-		
-main()
+	    # Wait for a connection
+	    print(sys.stderr, 'waiting for a connection')
+	    connection, client_address = listen_socket.accept()
+	    print("SS ", connection, client_address)
+	    option = Tcp_Read()
+	    option = option.strip('\n') 
+	    print(option)
+	    
+	    try:
+	        print(sys.stderr, 'connection from', client_address)
 
+	        # Receive the data in small chunks and retransmit it
+	        while True:
+	            data = connection.recv(16)
+	            print(sys.stderr, 'received "%s"' % data)
+	            if data:
+	                print(sys.stderr, 'sending data back to the client')
+	                connection.sendall(data)
+	            else:
+	                print(sys.stderr, 'no more data from', client_address)
+	                break
+	            
+	    finally:
+	        # Clean up the connection
+	    	connection.close()
+
+main()
